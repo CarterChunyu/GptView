27b18c8e9569ff54cf1086dc89ddc6acd2ba66dc
chunyu <qapo741@gmail.com>
2025-01-17 21:56:57 +0800

    init

---
+++ GptView/Controllers/GptController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GptView.Controllers
+{
+    public class GptController : Controller
+    {
+        private readonly IConfiguration _config;
+        public GptController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.url = _config["Url:Local"];
+            return View();
+        }
+    }
+}
