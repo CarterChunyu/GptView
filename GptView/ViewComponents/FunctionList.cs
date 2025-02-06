21f733d6f4999bac4289419c89ed80634bb35c31
m1218 <m1218@PADAA852>
2025-02-06 17:09:33 +0800

    更改Component設定

--- GptView/ViewComponents/FunctionList.cs
+++ GptView/ViewComponents/FunctionList.cs
@@ -10,7 +10,8 @@ namespace GptView.ViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var userInfo = HttpContext.GetObjectFromSession<UserInfoVM>("UserInfo");
-            userInfo.functionlist = userInfo.functionlist.Where(x => !x.isapi).ToList();
+            if(userInfo != null)
+                userInfo.functionlist = userInfo.functionlist.Where(x => !x.isapi).ToList();
             await Task.CompletedTask;
             return View(userInfo);
         }
